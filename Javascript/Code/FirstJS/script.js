e717019b48d3d0c5c56317965fedbad7b1998d2f
Shobhit <Shobhitkumar.Singh@d2kindia.com>
2024-12-20 07:40:33 +0530

    javascript changes

---
+++ Javascript/Code/FirstJS/script.js
@@ -0,0 +1,15 @@
+  function  fun()
+    {
+        if(true)
+            {
+                var x=25;
+            }
+        console.log(x);            
+
+    }
+
+    let a=+prompt("Enter the length ");
+    let b=+prompt("Enter the breadth ");
+    let area=a*b;
+    alert("The area of rectangle is"+area)
+
---
+++ Javascript/Code/primitive_data_types/Primitive_scripts.js
@@ -0,0 +1,16 @@
+let a=25;
+console.log(a);
+console.log(typeof a)
+console.log(a.toFixed(2))// fixed no of decimal digits
+console.log(a.toPrecision(3))// fixed no of digits
+console.log(a.toString(2))
+
+
+// Math Method
+console.log(Math.max(5,8,2,5,3))
+console.log(Math.min(5,8,2,5,3))
+console.log(Math.sqrt(16))
+console.log(Math.cbrt(27))
+console.log(Math.floor(2.4))
+console.log(Math.round(3.6))
+console.log(Math.random())
